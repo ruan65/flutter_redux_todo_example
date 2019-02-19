9d198a07c437e22a858552f9d38ec51a9073d52a
ruan65 <ruan65@gmail.com>
2019-02-19 16:40:22 +0300

    vm

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:todo_redux_example/model/model.dart';
+import 'package:todo_redux_example/state/app_state.dart';
+import 'package:todo_redux_example/state/redux/actions.dart';
+import 'package:todo_redux_example/state/redux/reducers.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: MyHomePage(),
+    final Store<AppState> store = Store<AppState>(
+      appStateReducer,
+      initialState: AppState.initialState(),
+    );
+
+    return StoreProvider<AppState>(
+      store: store,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: MyHomePage(),
+      ),
     );
   }
 }
@@ -29,7 +43,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Redux Items'),
       ),
-      body: Container(),
+      body: StoreConnector<AppState, _ViewModel>(),
+    );
+  }
+}
+
+class _ViewModel {
+  final List<Item> items;
+  final Function(String) onAddItem;
+  final Function(Item) onRemoveItem;
+  final Function() onRemoveAllItems;
+
+  _ViewModel({
+    this.items,
+    this.onAddItem,
+    this.onRemoveItem,
+    this.onRemoveAllItems,
+  });
+
+  factory _ViewModel.create(Store<AppState> store) {
+    _onAddItem(String body) {
+      store.dispatch(AddItemAction(body));
+    }
+
+    _onRemoveItem(Item item) {
+      store.dispatch(RemoveItemAction(item));
+    }
+
+    _onRemoveAllItem() {
+      store.dispatch(RemoveAllItemsAction());
+    }
+
+    return _ViewModel(
+      items: store.state.items,
+      onAddItem: _onAddItem,
+      onRemoveItem: _onRemoveItem,
+      onRemoveAllItems: _onRemoveAllItem,
     );
   }
 }
